43df8fa040ae05b4cf3b4a05749ba288dac0e01a
fcoyagami <fcoyagami@gmail.com>
2017-01-15 00:32:25 -0300

    Add files via upload

---
+++ paneo derecha a izquierda.rb
@@ -0,0 +1,45 @@
+# Paneo izquierda a derecha
+
+use_synth :beep
+play 60, pan: -1
+sleep 0.2
+play 60, pan: -0.9
+sleep 0.2
+play 60, pan: -0.8
+sleep 0.2
+play 60, pan: -0.7
+sleep 0.2
+play 60, pan: -0.6
+sleep 0.2
+play 60, pan: -0.5
+sleep 0.2
+play 60, pan: -0.4
+sleep 0.2
+play 60, pan: -0.3
+sleep 0.2
+play 60, pan: -0.2
+sleep 0.2
+play 60, pan: -0.1
+sleep 0.2
+play 60, pan: 0
+sleep 0.2
+play 60, pan: 0.1
+sleep 0.2
+play 60, pan: 0.2
+sleep 0.2
+play 60, pan: 0.3
+sleep 0.2
+play 60, pan: 0.4
+sleep 0.2
+play 60, pan: 0.5
+sleep 0.2
+play 60, pan: 0.6
+sleep 0.2
+play 60, pan: 0.7
+sleep 0.2
+play 60, pan: 0.8
+sleep 0.2
+play 60, pan: 0.9
+sleep 0.2
+play 60, pan: 1
+sleep 0.2
