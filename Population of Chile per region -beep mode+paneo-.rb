7825720bbbcda9cb39bc077f2e8b8f41509eea86
fcoyagami <fcoyagami@gmail.com>
2017-01-18 04:19:27 -0300

    Add files via upload

---
+++ Population of Chile per region -beep mode+paneo-.rb
@@ -0,0 +1,49 @@
+#Population of Chile per region -beep mode+paneo-
+
+use_synth :beep
+
+#Arica y Parinacota
+play 13, release: 2, amp: 5, pan: -1
+sleep 2
+#Tarapacá
+play 19, release: 2, amp: 5, pan: -0.8
+sleep 2
+#Antofagasta
+play 35, release: 2, amp: 5, pan: -0.7
+sleep 2
+#Atacama
+play 17, release: 2, amp: 5, pan: -0.5
+sleep 2
+#Coquimbo
+play 43, release: 2, amp: 5, pan: -0.4
+sleep 2
+#Valparaíso
+play 101, release: 2, amp: 5, pan: -0.3
+sleep 2
+#Metropolitana
+play 163, release: 2, amp: 5, pan: -0.2
+sleep 2
+#O’Higgins
+play 51, release: 2, amp: 5, pan: 0
+sleep 2
+#Maule
+play 58, release: 2, amp: 5, pan: 0.2
+sleep 2
+#Biobío
+play 117, release: 2, amp: 5, pan: 0.3
+sleep 2
+#La Araucanía
+play 55, release: 2, amp: 5,pan: 0.4
+sleep 2
+#Los Ríos
+play 22, release: 2, amp: 5, pan: 0.5
+sleep 2
+#Los Lagos
+play 47, release: 2, amp: 5, pan: 0.7
+sleep 2
+#Aysén
+play 6, release: 2, amp: 5, pan: 0.8
+sleep 2
+#Magallanes
+play 9, release: 2, amp: 5,pan: 1
+sleep 2
