887dfc7b1745931580c5ca412a1b915cf423eea5
fcoyagami <fcoyagami@gmail.com>
2017-01-18 04:26:32 -0300

    Add files via upload

---
+++ Population of Chile per region -sample mode + paneo-.rb
@@ -0,0 +1,48 @@
+#Population of Chile per region -sample mode + paneo-
+
+
+#Arica y Parinacota
+sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 1.3, pan: -1
+sleep 10
+#Tarapacá
+sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 1.9, pan: -0.8
+sleep 10
+#Antofagasta
+sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 3.5, pan: -0.7
+sleep 10
+#Atacama
+sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 1.7 , pan: -0.5
+sleep 10
+#Coquimbo
+sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 4.3, pan: -0.4
+sleep 10
+#Valparaíso
+sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 10.1, pan: -0.3
+sleep 10
+#Metropolitana
+sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 40.6, pan: -0.2
+sleep 10
+#O’Higgins
+sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 5.1, pan: 0
+sleep 10
+#Maule
+sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 5.8, pan: 0.2
+sleep 10
+#Biobío
+sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 11.7, pan: 0.3
+sleep 10
+#La Araucanía
+sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 5.5, pan: 0.4
+sleep 10
+#Los Ríos
+sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 2.2, pan: 0.5
+sleep 10
+#Los Lagos
+sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 4.7, pan: 0.7
+sleep 10
+#Aysén
+sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 0.6, pan: 0.8
+sleep 10
+#Magallanes
+sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 0.9,pan: 1
+sleep 10
