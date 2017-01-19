6cd9854dd68a992871409ee4c4b6300ad5ce8c11
fcoyagami <fcoyagami@gmail.com>
2017-01-18 23:09:03 -0300

    Update Population of Chile per region -sample mode + paneo-.rb

--- Population of Chile per region -sample mode + paneo-.rb
+++ Population of Chile per region -sample mode + paneo-.rb
@@ -1,10 +1,11 @@
 #Population of Chile per region -sample mode + paneo-
 
+#Para poner tu sampleo simplemente debes poner la ruta en donde se ubica tu archivo (ej. c:/usuario/escritorio/sonido/prueba.wav)
 
 #Arica y Parinacota
 sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 1.3, pan: -1
 sleep 10
-#Tarapacá
+#TarapacÃ¡
 sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 1.9, pan: -0.8
 sleep 10
 #Antofagasta
@@ -16,31 +17,31 @@ sleep 10
 #Coquimbo
 sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 4.3, pan: -0.4
 sleep 10
-#Valparaíso
+#ValparaÃ­so
 sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 10.1, pan: -0.3
 sleep 10
 #Metropolitana
 sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 40.6, pan: -0.2
 sleep 10
-#O’Higgins
+#Oâ€™Higgins
 sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 5.1, pan: 0
 sleep 10
 #Maule
 sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 5.8, pan: 0.2
 sleep 10
-#Biobío
+#BiobÃ­o
 sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 11.7, pan: 0.3
 sleep 10
-#La Araucanía
+#La AraucanÃ­a
 sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 5.5, pan: 0.4
 sleep 10
-#Los Ríos
+#Los RÃ­os
 sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 2.2, pan: 0.5
 sleep 10
 #Los Lagos
 sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 4.7, pan: 0.7
 sleep 10
-#Aysén
+#AysÃ©n
 sample "C:/Users/Francisco/Desktop/sonidos/gente.wav", amp: 0.6, pan: 0.8
 sleep 10
 #Magallanes
